2c9023dc59e024c165ea7e24736716caa0691957
jiseok-choi <jschoi2@wemb.co.kr>
2021-03-12 00:09:24 +0900

    [change] 영속성 테스트  - persistence.xml batch 옵션 추가  - flush 동작방식  - detach 관련 주석 추가

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -21,19 +21,22 @@ public class JpaMain {
         tx.begin();
 
         try {
+            // find를 통해서 데이터가 1차 캐시에 등록되면 영속 상태가 된다.
+            Member member = entityManager.find(Member.class, 150L);
 
-            // 비영속
-            Member member = new Member();
-            member.setId(100L);
-            member.setName("HelloJPA");
+            // 이후 데이터를 변경하면 컨텍스트가 변경을 감지하여 (더티체킹)
+            member.setName("ZZZZ");
 
-            // 영속
-            System.out.println("=== BEFORE ===");
-            entityManager.persist(member);
-            System.out.println("=== AFTER ===");
+            // 영속성 컨텍스트에서 떼어낸다!!
+            // jpa에서 관리를 하지 않는다!
+            entityManager.detach(member);
 
-            // 트랜잭션이 커밋 될때 영속성 컨텍스트에서 DB 에 쿼리가 전달된다.
-           tx.commit();
+            System.out.println("====================");
+
+            // 원래라면 이 시점에 업데이트 쿼리를 날려준다.
+            // 하지만 detach 를 수행한 이후에는 쿼리가 수행되지 않는다!
+            tx.commit();
+
         } catch (Exception e) {
             tx.rollback();
         } finally {
