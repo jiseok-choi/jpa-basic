3dda09a30ff29820af719099f09fdea7a9b0a29a
jiseok-choi <jschoi2@wemb.co.kr>
2021-04-03 21:07:22 +0900

    [add] 매핑 실전예제

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -12,38 +12,38 @@ public class JpaMain {
         // persistence.xml 파일의 속성
         // persistence-unit name 값을 바인딩 한다
         // 팩토리를 생성하는 순간 데이터베이스와 연결이 되었다는 의미이다.
-        EntityManagerFactory emf =  Persistence.createEntityManagerFactory("hello");
-
-        // EntityManagerFactory 에서 entityManger 꺼내기
-        EntityManager entityManager = emf.createEntityManager();
-
-        EntityTransaction tx = entityManager.getTransaction();
-        tx.begin();
-
-        try {
-            // find를 통해서 데이터가 1차 캐시에 등록되면 영속 상태가 된다.
-            Member member = entityManager.find(Member.class, 150L);
-
-            // 이후 데이터를 변경하면 컨텍스트가 변경을 감지하여 (더티체킹)
-            member.setName("ZZZZ");
-
-            // 영속성 컨텍스트에서 떼어낸다!!
-            // jpa에서 관리를 하지 않는다!
-            entityManager.detach(member);
-
-            System.out.println("====================");
-
-            // 원래라면 이 시점에 업데이트 쿼리를 날려준다.
-            // 하지만 detach 를 수행한 이후에는 쿼리가 수행되지 않는다!
-            tx.commit();
-
-        } catch (Exception e) {
-            tx.rollback();
-        } finally {
-            entityManager.close();
-
-            emf.close();
-        }
+//        EntityManagerFactory emf =  Persistence.createEntityManagerFactory("hello");
+//
+//        // EntityManagerFactory 에서 entityManger 꺼내기
+//        EntityManager entityManager = emf.createEntityManager();
+//
+//        EntityTransaction tx = entityManager.getTransaction();
+//        tx.begin();
+//
+//        try {
+//            // find를 통해서 데이터가 1차 캐시에 등록되면 영속 상태가 된다.
+//            Member member = entityManager.find(Member.class, 150L);
+//
+//            // 이후 데이터를 변경하면 컨텍스트가 변경을 감지하여 (더티체킹)
+//            member.setName("ZZZZ");
+//
+//            // 영속성 컨텍스트에서 떼어낸다!!
+//            // jpa에서 관리를 하지 않는다!
+//            entityManager.detach(member);
+//
+//            System.out.println("====================");
+//
+//            // 원래라면 이 시점에 업데이트 쿼리를 날려준다.
+//            // 하지만 detach 를 수행한 이후에는 쿼리가 수행되지 않는다!
+//            tx.commit();
+//
+//        } catch (Exception e) {
+//            tx.rollback();
+//        } finally {
+//            entityManager.close();
+//
+//            emf.close();
+//        }
 
 
 
